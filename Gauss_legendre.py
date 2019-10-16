3d80c791eba1ca3594671f2161842a9727bff5b2
martimc <martin_cars@hotmail.com>
2019-10-16 20:46:56 +0200

    Update Gauss_legendre.py

--- Gauss_legendre.py
+++ Gauss_legendre.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def function(x):
+def function(a, b, x):
     #alpha = 2
     #return np.exp(-2*alpha*x)
-    return 3*x**4
+    t = (b-a)*x/2 + (b+a)/2
+    return np.exp(-2*t)
 
 def Gauss_leg(n):
     L_N = np.zeros(n+1)
@@ -25,11 +26,28 @@ def Gauss_leg(n):
 
     return xi, w
 
-if __name__ == '__main__':
-    n = 3
-    xi, w = Gauss_leg(n)
+def Test_monte1d(b, a, n):
+    x = np.random.uniform(a,b,n)
+    fx = function(x)
+    crude_mc = sum(fx)
+    sigma = sum(fx*fx)
 
+    integral = crude_mc*(b-a)/n
+    return integral, sigma
+
+if __name__ == '__main__':
+    n = 100
+    lamb = 3
+    xi, w = Gauss_leg(n)
+    xi_new = np.tan(np.pi/4*(1+xi))
+    w_new = np.pi/4*w/(np.cos(np.pi/4*(1+xi))**2)
+
+
+    start = -lamb; end = lamb
     Leg_I = 0
     for i in range(n):
-        Leg_I += w[i]*function(xi[i])
+        Leg_I += w[i]*function(start, end, xi[i])
+    Leg_I *= (end-start)/2
     print(Leg_I)
+
+    #print(Test_monte1d(2, 0, 1000000))
