c20fd12b17db51e1e8ccce6c12e9e6b2e3af6e2e
Henrik Haugerud Carlsen <henrikhhc@gmail.com>
2019-10-17 21:15:28 +0200

    Update MonteCarloDone.py

--- MonteCarloDone.py
+++ MonteCarloDone.py
@@ -2,41 +2,36 @@ print('program begins')
 import numpy as np
 import matplotlib.pyplot as plt
 
-def function(x1,x2,y1,y2,z1,z2,N):
+def function(x1,x2,y1,y2,z1,z2,N): #Definging the function to be integrated
     alpha=2
     r1=np.sqrt(x1**2 + y1**2 + z1**2)
     r2=np.sqrt(x2**2 + y2**2 + z2**2)
 
     denominator = np.sqrt((x2-x1)*(x2-x1) +(y2-y1)*(y2-y1) + (z2- z1)*(z2- z1))
-
+    #Returning only the function values where denominator <10^4
     return np.where(denominator < 1e-4,0, np.exp(-2*alpha*(r1 + r2)) /denominator)
-    """
-    if denominator < 1e-4:
-        return 0
-
-    else:
-        return np.exp(-2*alpha*(r1 + r2)) /denominator
-    """
 
 def MonteCarloNormal(N,lmbda):
-
+    #integral limits
     a=-lmbda
     b= lmbda
 
+    #Making uniform and random distribution of N points from -lambda to lambda for all variables
+    x1 = np.random.uniform(a,b,N)#*(b-a) + a
+    y1 = np.random.uniform(a,b,N)#*(b-a) + a
+    z1 = np.random.uniform(a,b,N)#*(b-a) + a
 
-    x1 = np.random.uniform(0,1,N)*(b-a) + a
-    y1 = np.random.uniform(0,1,N)*(b-a) + a
-    z1 = np.random.uniform(0,1,N)*(b-a) + a
+    x2 = np.random.uniform(a,b,N)#*(b-a) + a
+    y2 = np.random.uniform(a,b,N)#*(b-a) + a
+    z2 = np.random.uniform(a,b,N)#*(b-a) + a
 
-    x2 = np.random.uniform(0,1,N)*(b-a) + a
-    y2 = np.random.uniform(0,1,N)*(b-a) + a
-    z2 = np.random.uniform(0,1,N)*(b-a) + a
-
+    #allocating constant to function()
     fx=function(x1,x2,y1,y2,z1,z2,N)
 
-    crude_mc = np.sum(fx)/N
+    crude_mc = np.sum(fx)/N #summing
 
-    integral=(crude_mc)*(b-a)**6
+    integral=(crude_mc)*(b-a)**6 #accounting for 6 dimensions by multiplying with (b-a)^6
+
 #    standard_dev = np.sqrt(np.sum(func_values - mean_value)**2)/(N)
 
 #    total_mean_value = np.sum(mean_value)
