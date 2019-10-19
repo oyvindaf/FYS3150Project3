77446286ee38d33cf1bc6c5a0033602882e15857
martimc <martin_cars@hotmail.com>
2019-10-19 21:17:42 +0200

    Update Gauss_legendre.py

--- Gauss_legendre.py
+++ Gauss_legendre.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def integrand(a, b, x1, x2, y1, y2, z1, z2):
-    tol = 1e-10
+    tol = 1e-14
     x1 = (b-a)*x1/2 + (b+a)/2
     x2 = (b-a)*x2/2 + (b+a)/2
     y1 = (b-a)*y1/2 + (b+a)/2
@@ -22,6 +22,26 @@ def integrand(a, b, x1, x2, y1, y2, z1, z2):
     else:
         I = 0
     return I
+
+def polar_integrand(a1, b1, a2, b2, r1, r2, t1, t2, p1, p2):
+    tol = 1e-14
+
+    t1 = (b1 - a1) * t1 / 2 + (b1 + a1) / 2
+    t2 = (b1 - a1) * t2 / 2 + (b1 + a1) / 2
+    p1 = (b2 - a2) * p1 / 2 + (b2 + a2) / 2
+    p2 = (b2 - a2) * p2 / 2 + (b2 + a2) / 2
+
+    cosb = np.cos(t1)*np.cos(t2) + np.sin(t1)*np.sin(t2)*np.cos(p1-p2)
+
+    r12 = np.sqrt(r1**2+r2**2-2*r1*r2*cosb)
+
+    if r12 > tol:
+        I = np.exp(-4*(r1+r2))*r1**2*r2**2*np.sin(t1)*np.sin(t2)/r12
+    else:
+        I = 0
+    return I
+
+
 
 def Gauss_leg(n):
     L_N = np.zeros(n+1)
@@ -57,11 +77,11 @@ def Gauss_lag(n):
     return xi, w
 
 if __name__ == '__main__':
-    n = np.linspace(3,15,7)
+    n = np.linspace(3,9,4)
     Leg_I = np.zeros(len(n))
     lamb = 3
 
-    '''
+
     for g in range(len(n)):
         N = int(n[g])
         xi, w = Gauss_leg(N)
@@ -85,7 +105,29 @@ if __name__ == '__main__':
     print(Leg_I, 5*np.pi**2/16**2)
     plt.plot(n, Leg_I)
     plt.show()
-    '''
 
-    xi, wi = Gauss_lag(5)
-    print(xi, wi)
+
+    # laguerre part
+
+    Lag_I = np.zeros(len(n))
+    at = 0; bt = np.pi
+    ap = 0; bp = 2 * np.pi
+
+    for g in range(len(n)):
+        N = int(n[g])
+        ri, w_ri = Gauss_lag(N)
+        anglei, w_anglei = Gauss_leg(N)
+
+        for i in range(N):
+            for j in range(N):
+                for k in range(N):
+                    for l in range(N):
+                        for m in range(N):
+                            for h in range(N):
+                                Lag_I[g] += w_ri[i] * w_ri[j] * w_anglei[k] * w_anglei[l] * w_anglei[m] * w_anglei[h] \
+                                        * polar_integrand(at, bt, ap, bp, ri[i], ri[j], anglei[k], anglei[l], anglei[m], anglei[h])
+            print(i)
+        Lag_I[g] *= ((bt - at) / 2) ** 2 * ((bp - ap)/2)**2
+    print(Lag_I)
+    plt.plot(n, Lag_I)
+    plt.show()
