fa87b2cc7dd5046df847d0b5c47f56e0e4f89b33
martimc <martin_cars@hotmail.com>
2019-10-22 16:14:03 +0200

    Update Gauss_legendre.py

--- Gauss_legendre.py
+++ Gauss_legendre.py
@@ -5,8 +5,11 @@ import time
 from numba import jit
 
 def integrand(a, b, x1, x2, y1, y2, z1, z2):
+    '''
+    Cartesian integral of the six dimensional case with limits a = -lambda and b = lambda
+    '''
     tol = 1e-10
-    x1 = (b-a)*x1/2 + (b+a)/2
+    x1 = (b-a)*x1/2 + (b+a)/2  # change of variables for [-1, 1] to [a, b]
     x2 = (b-a)*x2/2 + (b+a)/2
     y1 = (b-a)*y1/2 + (b+a)/2
     y2 = (b-a)*y2/2 + (b+a)/2
@@ -18,13 +21,16 @@ def integrand(a, b, x1, x2, y1, y2, z1, z2):
 
     distance = np.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2))
 
-    if distance > tol:
+    if distance > tol:  # checking for singularities
         I = np.exp(-4*(r1+r2))/distance
     else:
         I = 0
     return I
 
 def polar_integrand(a1, b1, a2, b2, r1, r2, t1, t2, p1, p2):
+    """
+    The spherical integration function which returns the function value of the six dimensional point
+    """
     tol = 1e-10
 
     t1 = (b1-a1)*t1/2 + (b1+a1)/2
@@ -45,6 +51,9 @@ def polar_integrand(a1, b1, a2, b2, r1, r2, t1, t2, p1, p2):
 
 
 def Gauss_leg(n):
+    """
+    Function which finds the roots and weights of the n-th legendre polynomial.
+    """
     L_N = np.zeros(n+1)
     L_N[-1] = 1
     xi = np.polynomial.legendre.legroots(L_N)
@@ -62,6 +71,9 @@ def Gauss_leg(n):
     return xi, w
 
 def Gauss_lag(n):
+    """
+    Function which finds the roots and weights of the n-th laguerre polynomial.
+    """
     L_N = np.zeros(n+1)
     L_N[-1] = 1
     xi = np.polynomial.laguerre.lagroots(L_N)
@@ -79,7 +91,6 @@ def Gauss_lag(n):
 
 if __name__ == '__main__':
     n = np.linspace(3,25,12)
-    #n = np.linspace(3,7,3)
     Leg_I = np.zeros(len(n))
     lamb = 3
     time_spent_leg = np.zeros(len(n))
@@ -92,7 +103,7 @@ if __name__ == '__main__':
         a = -lamb
         b = lamb
         start = time.time()
-        for i in range(N):
+        for i in range(N):  # the six-doubled for-loop to find the integration value
             for j in range(N):
                 for k in range(N):
                     for l in range(N):
@@ -100,11 +111,11 @@ if __name__ == '__main__':
                             for h in range(N):
                                 Leg_I[g] += w[i]*w[j]*w[k]*w[l]*w[m]*w[h]*integrand(a, b, xi[i], xi[j], xi[k], xi[l], xi[m], xi[h])
             print((i+1)/N*100)
+        
         Leg_I[g] *= ((b-a)/2)**6
         end = time.time()
         time_spent_leg[g] = end-start
 
-    print(Leg_I, 5*np.pi**2/16**2)
     plt.plot(n, Leg_I, 'bo-', label='Gauss Legendre for Cartesian')
 
 
@@ -118,11 +129,10 @@ if __name__ == '__main__':
     for g in range(len(n)):
         N = int(n[g])
         ri, w_ri = Gauss_lag(N)
-        print(N, ri, w_ri)
         angle, w_angle = Gauss_leg(N)
 
         start = time.time()
-        for i in range(N):
+        for i in range(N):  # the six-doubled for-loop to find the integration value
             for j in range(N):
                 for k in range(N):
                     for l in range(N):
@@ -142,11 +152,8 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
-    file = open('solution_GQ.txt', 'w')
+    file = open('solution_GQ.txt', 'w') #  Writing our solutions to a text file
 
     for i in range(len(n)):
         file.write('N=%g, Gauss_leg = %g, time = %g, Gauss_lag = %g, time = %g \n' % (n[i], Leg_I[i], time_spent_leg[i],
                                                                                       Lag_I[i], time_spent_lag[i]))
-    print(n)
-    print(time_spent_leg)
-    print(time_spent_lag)
